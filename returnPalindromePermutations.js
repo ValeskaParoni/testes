8a77d6ee0e970faef34c3e2dce90a0be4f0d19b3
Valeska Paroni <valeskaparoni@gmail.com>
2018-10-20 13:46:15 -0300

    Update returnPalindromePermutations.js

--- returnPalindromePermutations.js
+++ returnPalindromePermutations.js
@@ -6,10 +6,20 @@ let allPossibleCombinations = function(prefix, string){
     for (let letter of string){
       let newString = string.replace(letter, '');
       let newPrefix = prefix + letter;
-      combinations.push(allPossibleCombinations(newPrefix, newString));
+      combinations = combinations.concat(allPossibleCombinations(newPrefix, newString));
     }
     return combinations;
   }
+}
+
+let reverseString = function(string){
+    let length = string.lentgh;
+    for (let i; i < length/2; i++){
+        let aux = string[i];
+        string[i] = string[length-i-1];
+        string[length-i-1] = aux;
+    }
+    return string;
 }
 
 let returnPalindromePermutations = function(string){
@@ -17,30 +27,24 @@ let returnPalindromePermutations = function(string){
     let doubledLetters = '';
     for (let index in string) {
         let letter = string[index];
-        console.log('viewing letter ', letter);
-        console.log('alone letters are ', aloneLetters);
-        console.log('doubled letter are ', doubledLetters);
         if (aloneLetters.indexOf(letter)==-1){
             aloneLetters += letter;
-            console.log('alone letter added up');
-            console.log('alone letters became ', aloneLetters);
         }
         else {
             aloneLetters = aloneLetters.replace(letter, '');
             doubledLetters += letter;
-            console.log('double letter added up');
-            console.log('alone letters became ', aloneLetters);
-            console.log('doubled letters became ', doubledLetters);
         }
     }
     if (aloneLetters.length > 1) return [];
-    console.log('finish: doubled letters are ', doubledLetters);
-    console.log('and alone letters are ', aloneLetters);
-    return allPossibleCombinations('', doubledLetters);
+    let combinations = allPossibleCombinations('', doubledLetters);
+    let palindromos = [];
+    for (let combination of combinations) {
+        let palindromo = combination + aloneLetters + reverseString(combination);
+        palindromos.push(palindromo);
+    }
+    return palindromos;
 }
 
+/*
 console.log(returnPalindromePermutations('tewrswriite'));
-/*console.log(isPalindromePermutation('aaaaba'));
-console.log(isPalindromePermutation('testq'));
-console.log(isPalindromePermutation('ttsqq'));
 */
