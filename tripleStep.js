9394b8dde8aa59d9fbb168e74ad7b1b544bf75f1
Valeska Paroni <valeskaparoni@gmail.com>
2018-10-20 21:07:18 -0300

    Update tripleStep.js

--- tripleStep.js
+++ tripleStep.js
@@ -1,6 +1,7 @@
 let countHowManyWays = function(n){
-    if (n==1) return 3;
-    else return 3*countHowManyWays(n-1);
+    if (n<0) return 0;
+    if (n==0) return 1;
+    return countHowManyWays(n-1) + countHowManyWays(n-2) + countHowManyWays(n-3);
 }
 
 console.log(countHowManyWays(4));
